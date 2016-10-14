6c6ad44c6aace65c0865f05b69f7ffee3bc766d1
Ayush <ayush15may@gmail.com>
2016-10-14 22:20:58 +0530

    images styled, now I need to add scrolling event

--- src/app/images-container/images-container.component.ts
+++ src/app/images-container/images-container.component.ts
@@ -8,43 +8,49 @@ import {ViewChild} from "@angular/core/src/metadata/di";
   styleUrls: ['./images-container.component.css']
 })
 export class ImagesContainerComponent implements OnInit, AfterViewInit {
-  @ViewChild('infinteList') infiniteList:any;
+  @ViewChild('infinteList') infiniteList: any;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
   ngAfterViewInit() {
-    this.loadMore();
+    this.detectBottom();
   }
 
 
-  private loadMore(){
+  private detectBottom() {
     let listElm = this.infiniteList.nativeElement;
     let nextItem = 1;
-    // for (var i = 0; i < 10; i++) {
-    //   let item = document.createElement('li');
-    //   item.innerText = 'Item ' + nextItem++;
-    //   listElm.appendChild(item);
-    // }
-    for (var i = 0; i < 10; i++) {
-      let item = document.createElement('img');
-      //item.setAttribute('src', 'http://lorempixel.com/50/50/animals/');
-      item.src = 'src/app/images-container/img11.JPG';
-      listElm.appendChild(item);
-    }
-    console.log("get element",listElm);
+    function loadMore() {
+      // for (var i = 0; i < 10; i++) {
+      //   let item = document.createElement('li');
+      //   item.innerText = 'Item ' + nextItem++;
+      //   listElm.appendChild(item);
+      // }
+      for (var i = 0; i < 10; i++) {
+        if(nextItem==11){
+          nextItem =1;
+        }
+        let item = document.createElement('img');
+        //item.setAttribute('src', 'http://lorempixel.com/50/50/animals/');
+        item.src = 'https://raw.githubusercontent.com/ayu15/InfiniteImages/master/src/assets/images/img' + nextItem++ + '.jpg';
+        item.className = "myImages";
+        item.style = "width:40%; margin: 5%";
+        listElm.appendChild(item);
+      }
+    };
 
+    loadMore();
+    listElm.addEventListener('scroll', function () {
+      if (listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight) {
+        loadMore();
+      }
+    });
   }
-
-
-// // Detect when scrolled to bottom.
-//   private listElm.addEventListener('scroll', function() {
-//   if (listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight) {
-//     loadMore();
-//   }
-// });
 
 
 }
+
